8626ca5e6fb1df274550785eb5bb9c0c822376ee
Everton Santos <e.santos081992@gmail.com>
2021-06-24 20:30:22 -0300

    Podcastr

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/global.scss';
+import { Header } from '../components/Header';
+import styles from '../styles/app.module.scss';
+import { Player } from '../components/Player'
+import { PlayerContext } from '../contexts/PlayerContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+
+    <PlayerContext.Provider value={'Everton'}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
+
+  )
+}
+
+export default MyApp
